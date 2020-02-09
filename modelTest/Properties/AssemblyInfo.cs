3e84914be59c5182874a2383e3b4be38046df9bc
gomezLF <gomez_pipe99@hotmail.com>
2020-02-09 13:07:09 -0500

    Creado las clases y el unitTestProject

---
+++ modelTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("modelTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("modelTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("212a5c3e-b72a-4241-9350-f4d870347723")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ modelTest/SortingMethodTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace modelTest
+{
+    [TestClass]
+    public class SortingMethodTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
