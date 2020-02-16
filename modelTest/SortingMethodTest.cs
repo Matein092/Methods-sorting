2ccbc507f7cb30530397e91e14be5400a8f39972
gomezLF <gomez_pipe99@hotmail.com>
2020-02-16 18:37:46 -0500

    Se arreglaron los escenarios para un mejor rendimiento a la hora de probar los algoritmos. Se crearon los test para cada algoritmo de ordenamiento, con las veces que se establecio.

--- modelTest/SortingMethodTest.cs
+++ modelTest/SortingMethodTest.cs
@@ -11,6 +11,10 @@ namespace modelTest
         private SortingMethod SortingMethod;
         private DataGenerator DataGenerator;
 
+        private int[] disorganized;
+        private int[] ascending;
+        private int[] descending;
+
         private int[] disorganizedArray;
         private int[] ascendingArray;
         private int[] descendingArray;
@@ -24,55 +28,318 @@ namespace modelTest
         {
             SetUpScenary0();
 
-            disorganizedArray = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_ONE);
-            ascendingArray = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_ONE);
-            descendingArray = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_ONE);
+            this.disorganized = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_ONE);
+            this.disorganizedArray = this.disorganized;
+
+            this.ascending = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_ONE);
+            this.ascendingArray = ascending;
+
+            this.descending = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_ONE);
+            this.descendingArray = descending;
+
+            Array.Sort(disorganized);
+            Array.Sort(ascending);
+            Array.Sort(descending);
         }
         private void SetUpScenary2()
         {
             SetUpScenary0();
 
-            disorganizedArray = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_TWO);
-            ascendingArray = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_TWO);
-            descendingArray = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_TWO);
+            this.disorganized = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_TWO);
+            this.disorganizedArray = this.disorganized;
+
+            this.ascending = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_TWO);
+            this.ascendingArray = ascending;
+
+            this.descending = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_TWO);
+            this.descendingArray = descending;
+
+            Array.Sort(disorganized);
+            Array.Sort(ascending);
+            Array.Sort(descending);
         }
         private void SetUpScenary3()
         {
             SetUpScenary0();
 
-            disorganizedArray = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_THREE);
-            ascendingArray = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_THREE);
-            descendingArray = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_THREE);
+            this.disorganized = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_THREE);
+            this.disorganizedArray = this.disorganized;
+
+            this.ascending = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_THREE);
+            this.ascendingArray = ascending;
+
+            this.descending = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_THREE);
+            this.descendingArray = descending;
+
+            Array.Sort(disorganized);
+            Array.Sort(ascending);
+            Array.Sort(descending);
         }
         private void SetUpScenary4()
         {
             SetUpScenary0();
 
-            disorganizedArray = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_FOUR);
-            ascendingArray = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_FOUR);
-            descendingArray = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_FOUR);
+            this.disorganized = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_FOUR);
+            this.disorganizedArray = this.disorganized;
+
+            this.ascending = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_FOUR);
+            this.ascendingArray = ascending;
+
+            this.descending = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_FOUR);
+            this.descendingArray = descending;
+
+            Array.Sort(disorganized);
+            Array.Sort(ascending);
+            Array.Sort(descending);
         }
         private void SetUpScenary5()
         {
             SetUpScenary0();
 
-            disorganizedArray = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_FIVE);
-            ascendingArray = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_FIVE);
-            descendingArray = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_FIVE);
+            this.disorganized = DataGenerator.GenerateDataRandomly(DataGenerator.TEN_TO_FIVE);
+            this.disorganizedArray = this.disorganized;
+
+            this.ascending = DataGenerator.GenerateDataIncreasingly(DataGenerator.TEN_TO_FIVE);
+            this.ascendingArray = ascending;
+
+            this.descending = DataGenerator.GenerateDataDecreasingly(DataGenerator.TEN_TO_FIVE);
+            this.descendingArray = descending;
+
+            Array.Sort(disorganized);
+            Array.Sort(ascending);
+            Array.Sort(descending);
         }
        
 
-
+        //Pruebas para insertion y selction sort con un array de tamano 10^1
         [TestMethod]
         public void InsertionSortTes()
         {
+            SetUpScenary1();
+
+            this.SortingMethod.InsertionSort(this.disorganizedArray);
+            this.SortingMethod.InsertionSort(this.ascendingArray);
+            this.SortingMethod.InsertionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_ONE);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_ONE);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_ONE);
+
+            
+            for (int i = 0; i < DataGenerator.TEN_TO_ONE; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
         }
 
         [TestMethod]
         public void SelectionSortTest()
         {
+            SetUpScenary1();
+
+            this.SortingMethod.SelectionSort(this.disorganizedArray);
+            this.SortingMethod.SelectionSort(this.ascendingArray);
+            this.SortingMethod.SelectionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_ONE);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_ONE);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_ONE);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_ONE; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
         }
 
+        //Pruebas para insertion y selction sort con un array de tamano 10^2
+        public void InsertionSortTes_2()
+        {
+            SetUpScenary2();
 
+            this.SortingMethod.InsertionSort(this.disorganizedArray);
+            this.SortingMethod.InsertionSort(this.ascendingArray);
+            this.SortingMethod.InsertionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_TWO);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_TWO);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_TWO);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_TWO; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
+
+        [TestMethod]
+        public void SelectionSortTest_2()
+        {
+            SetUpScenary2();
+
+            this.SortingMethod.SelectionSort(this.disorganizedArray);
+            this.SortingMethod.SelectionSort(this.ascendingArray);
+            this.SortingMethod.SelectionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_TWO);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_TWO);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_TWO);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_TWO; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
+
+        //Pruebas para insertion y selction sort con un array de tamano 10^3
+        public void InsertionSortTes_3()
+        {
+            SetUpScenary3();
+
+            this.SortingMethod.InsertionSort(this.disorganizedArray);
+            this.SortingMethod.InsertionSort(this.ascendingArray);
+            this.SortingMethod.InsertionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_THREE);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_THREE);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_THREE);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_THREE; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
+
+        [TestMethod]
+        public void SelectionSortTest_3()
+        {
+            SetUpScenary3();
+
+            this.SortingMethod.SelectionSort(this.disorganizedArray);
+            this.SortingMethod.SelectionSort(this.ascendingArray);
+            this.SortingMethod.SelectionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_THREE);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_THREE);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_THREE);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_THREE; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
+
+        //Pruebas para insertion y selction sort con un array de tamano 10^4
+        public void InsertionSortTes_4()
+        {
+            SetUpScenary4();
+
+            this.SortingMethod.InsertionSort(this.disorganizedArray);
+            this.SortingMethod.InsertionSort(this.ascendingArray);
+            this.SortingMethod.InsertionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_FOUR);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_FOUR);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_FOUR);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_FOUR; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
+
+        [TestMethod]
+        public void SelectionSortTest_4()
+        {
+            SetUpScenary4();
+
+            this.SortingMethod.SelectionSort(this.disorganizedArray);
+            this.SortingMethod.SelectionSort(this.ascendingArray);
+            this.SortingMethod.SelectionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_FOUR);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_FOUR);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_FOUR);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_FOUR; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
+
+        //Pruebas para insertion y selction sort con un array de tamano 10^4
+        public void InsertionSortTes_5()
+        {
+            SetUpScenary5();
+
+            this.SortingMethod.InsertionSort(this.disorganizedArray);
+            this.SortingMethod.InsertionSort(this.ascendingArray);
+            this.SortingMethod.InsertionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_FIVE);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_FIVE);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_FIVE);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_FIVE; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
+
+        [TestMethod]
+        public void SelectionSortTest_5()
+        {
+            SetUpScenary5();
+
+            this.SortingMethod.SelectionSort(this.disorganizedArray);
+            this.SortingMethod.SelectionSort(this.ascendingArray);
+            this.SortingMethod.SelectionSort(this.descendingArray);
+
+            //Aqui se verifica que en el proceso de ordenamiento el array mantenga su tamano
+            Assert.IsTrue(disorganizedArray.Length == DataGenerator.TEN_TO_FIVE);
+            Assert.IsTrue(ascendingArray.Length == DataGenerator.TEN_TO_FIVE);
+            Assert.IsTrue(descendingArray.Length == DataGenerator.TEN_TO_FIVE);
+
+
+            for (int i = 0; i < DataGenerator.TEN_TO_FIVE; i++)
+            {
+                Assert.IsTrue(disorganizedArray[i] == disorganized[i]);
+                Assert.IsTrue(ascendingArray[i] == ascending[i]);
+                Assert.IsTrue(descendingArray[i] == descending[i]);
+            }
+        }
     }
 }
